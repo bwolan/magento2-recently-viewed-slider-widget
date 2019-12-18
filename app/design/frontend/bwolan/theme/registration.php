3e4aea8c5f96a5c597c6bc165c0e0989cf7c3d99
bwolan <bartlomiej.wolan@gate-software.com>
2019-12-18 13:37:30 +0100

    Initial commit

---
+++ app/design/frontend/bwolan/theme/registration.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+
+ * Copyright � 2015 Magento. All rights reserved.
+
+ * See COPYING.txt for license details.
+
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+
+    'frontend/bwolan/theme',
+
+    __DIR__
+
+);
